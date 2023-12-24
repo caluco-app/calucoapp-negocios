73ba7668a94c2ad432329fdd3f20c0ea579af243
luis-guzman-97 <luisespinoza.le108@gamil.com>
2023-12-23 18:22:33 -0600

    pantalla de inventario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { NegociosComponent } from './pages/negocios/negocios.component';
 import { NegocioComponent } from './pages/negocio/negocio.component';
 import { ProductosComponent } from './pages/productos/productos.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
 
 export const routes: Routes = [
     {
@@ -24,5 +25,9 @@ export const routes: Routes = [
     {
         path: 'negocios/:idnegocio/productos',
         component: ProductosComponent
+    },
+    {
+        path: 'negocios/:idnegocio/inventarios',
+        component: InventarioComponent
     }
 ];
---
+++ src/app/pages/inventario/inventario.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { NegocioService } from '../../services/negocio.service';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+declare let $:any;
+
+@Component({
+  selector: 'app-inventario',
+  standalone: true,
+  imports: [NavbarComponent, CommonModule, FooterComponent, RouterModule],
+  providers:[NegocioService],
+  templateUrl: './inventario.component.html',
+  styleUrl: './inventario.component.scss'
+})
+export class InventarioComponent {
+  userData: any;
+  cappn_userkey: any;
+  idNegocio: any;
+  dataNegocio: any;
+  productos:any[]=[];
+
+  constructor( private router: Router, private authapi: AuthService, private route: ActivatedRoute, private negocioService:NegocioService) {
+    this.route.paramMap.subscribe(params => {
+      this.idNegocio = params.get('idnegocio'); // 'id' debe coincidir con el nombre del parámetro en tu ruta
+      console.log('ID obtenido:', this.idNegocio);
+
+      // Puedes realizar otras operaciones con el ID aquí, como llamar a servicios, etc.
+    });
+
+  }
+
+  ngOnInit() {
+    let session: any = sessionStorage.getItem('cappn_userkey');
+    this.cappn_userkey = JSON.parse(session);
+    this.validarUsuarioNegocio();
+  }
+  
+  abrirModalFormulario(){
+    $('#modalFormInventario').modal('show');
+  }
+
+
+  validarUsuarioNegocio() {
+    let data: any = { idusuario: this.cappn_userkey.usuario[0].id, idnegocio: this.idNegocio };
+    this.authapi.validarUsuarioNegocio(data).subscribe(response => {
+      console.log(response);
+      this.dataNegocio = response.data;
+      this.productosPorNegocios();
+    });
+  }
+
+  productosPorNegocios(){
+    this.negocioService.productosPorNegocios(this.idNegocio).subscribe(response => {
+      console.log(response);
+      this.productos = response.data;
+    });
+  }
+}
