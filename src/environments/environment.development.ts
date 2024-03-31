affc1c5d8082eab09ede7e79c717fa04d39f2efc
luis-guzman-97 <luisespinoza.le108@gamil.com>
2024-03-31 15:50:09 -0600

    Actualizando ruta

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-     API_URL_API: `http://localhost:4201/api`
+     API_URL_API: `http://20.55.36.102:4201/api`
     //API_URL_API: `https://px95dgsl-4201.use.devtunnels.ms/api`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API_URL_API: `https://px95dgsl-4201.use.devtunnels.ms/api`,
+    API_URL_API: `http://20.55.36.102:4201/api`,
 };
